9864bf5bca5aeaca8f5e0fa81f9a3a4121412854
Lavakumar999 <lavakumarfire999@gmail.com>
2021-01-24 15:15:15 +0530

    updated heroku

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 const app =express();
 
-app.use(express.static(__dirname+'/dist/e-commerce-application'));
+app.use(express.static(__dirname+'/dist'));
+app.listen(process.env.PORT || 8080);
 app.get('/*',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/e-commerce-application/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 
 });
-app.listen(process.env.PORT || 8080);
