9c4bb9127a83011d6f0ab2544c420598d7de178d
Lavakumar999 <lavakumarfire999@gmail.com>
2021-01-24 14:37:29 +0530

    updated heroku

--- src/server.js
+++ src/server.js
@@ -7,4 +7,4 @@ app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/e-commerce-application/index.html'));
 
 });
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
